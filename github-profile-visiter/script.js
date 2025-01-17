dcd305791d808b41448db111e7c680e899fa869a
Akmal Ali Jasmin <jasminaaa.20@uom.lk>
2025-01-17 03:32:41 -0800

    Create script.js

---
+++ github-profile-visiter/script.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name        New script github.com
+// @namespace   Violentmonkey Scripts
+// @match       https://github.com/{your username}*
+// @grant       none
+// @version     1.0
+// @author      -
+// @description 17/01/2025, 16:48:13
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Refresh the page every 5 seconds
+    setInterval(() => location.reload(), 3000);
+})();
